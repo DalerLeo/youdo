a2b605b83a5b17ac97bc9723662af18e8f899ba0
setuz <set.uzb@gmail.com>
2017-03-25 23:32:31 +0500

    WITH LOADING

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -4,10 +4,12 @@ import {Form} from 'semantic-ui-react'
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Checkbox from 'material-ui/Checkbox';
+import LinearProgress from 'material-ui/LinearProgress';
 
 const style = {
-    'margin-bottom':0
+    'margin-bottom': 0
 };
+
 const checkboxStyle = {
     'text-align': 'left',
     'margin-bottom': '10px',
@@ -20,47 +22,54 @@ const inputStyle = {
 
 const LabelFocusStyle = {
     marginTop: 0,
-    color:'#2196f3',
-    borderColor:'#2196f3'
+    color: '#2196f3',
+    borderColor: '#2196f3'
 };
 
 const SignInForm = (props) => {
-    return (
-        <Form loading={props.isLoading} onSubmit={props.onSubmit}>
-            <div className="signInHeader">
-                <div className="signInTitle">
-                    ВХОД В СИСТЕМУ
+    if (props.isLoading) {
+        return (
+            <LinearProgress mode="indeterminate" />
+        )
+    } else {
+        return (
+            <Form onSubmit={props.onSubmit}>
+                <div className="signInHeader">
+                    <div className="signInTitle">
+                        ВХОД В СИСТЕМУ
+                    </div>
                 </div>
-            </div>
-            <div className="signInInputs">
-                <TextField
-                    floatingLabelText="Email"
-                    name="username"
-                    fullWidth="true"
-                    style={{height:62}}
-                    floatingLabelFocusStyle={LabelFocusStyle}
-                    floatingLabelStyle={inputStyle}
-                />
-                <TextField
-                    floatingLabelText="Пароль"
-                    name="password"
-                    fullWidth="true"
-                    type="password"
-                    style={{height:62}}
-                    floatingLabelFocusStyle={LabelFocusStyle}
-                    floatingLabelStyle={inputStyle}
-                />
+                <div className="signInInputs">
+                    <TextField
+                        floatingLabelText="Email"
+                        name="username"
+                        fullWidth="true"
+                        style={{height: 62}}
+                        floatingLabelFocusStyle={LabelFocusStyle}
+                        floatingLabelStyle={inputStyle}
+                    />
+                    <TextField
+                        floatingLabelText="Пароль"
+                        name="password"
+                        fullWidth="true"
+                        type="password"
+                        style={{height: 62}}
+                        floatingLabelFocusStyle={LabelFocusStyle}
+                        floatingLabelStyle={inputStyle}
+                    />
 
-                <Checkbox
-                    style={checkboxStyle}
-                    name="rememberMe"
-                    label="Запомнить меня"
-                />
+                    <Checkbox
+                        style={checkboxStyle}
+                        name="rememberMe"
+                        label="Запомнить меня"
+                    />
 
-                <RaisedButton  type="submit" backgroundColor='#44637d' labelColor="#fff" label="Войти" fullWidth={true} />
-            </div>
-        </Form>
-    )
+                    <RaisedButton type="submit" backgroundColor='#44637d' labelColor="#fff" label="Войти"
+                                  fullWidth={true}/>
+                </div>
+            </Form>
+        )
+    }
 }
 
 export default SignInForm
