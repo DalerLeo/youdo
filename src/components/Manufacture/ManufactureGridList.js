d4b32e164365ff639912fb1c9b40fc04609997e0
leepree <6271527>
2017-05-03 14:49:48 +0500

    new changes

--- src/components/Manufacture/ManufactureGridList.js
+++ src/components/Manufacture/ManufactureGridList.js
@@ -50,6 +50,8 @@ const listHeader = [
     }
 ]
 
+
+
 const enhance = compose(
     injectSheet({
         addButton: {
@@ -62,7 +64,105 @@ const enhance = compose(
             top: '10px',
             right: '0',
             marginBottom: '0px'
+        },
+        productionMainRow:{
+            margin: '0 -26px;',
+            fontSize: '13px',
+            extend: 'productionMainRow',
+            '& a': {
+                color: 'rgb(18, 170, 235)'
+            }
+        },
+        productionLeftSide: {
+            background: '#fcfcfc',
+            borderRight: '1px solid #efefef',
+            height: 'calc(100vh - 65px)',
+            padding: '0'
+        },
+        productionRightSide: {
+            background: '#fff',
+            height: 'calc(100vh - 65px)',
+            padding: '0 30px'
+        },
+        productionUl: {
+            listStyle: 'none',
+            margin: '0',
+            padding: '0',
+            extend: 'productionUl',
+            '& li:last-child': {
+                border:'none'
+            }
+        },
+        productionTypeLi: {
+            margin: '0',
+            padding: '20px 30px',
+            borderBottom: '1px solid #efefef'
+        },
+        productionH2: {
+            fontSize: '13px',
+            fontWeight: 'bold',
+            margin: '0',
+            borderBottom: '1px solid #efefef',
+            padding: '20px 30px'
+        },
+        productionRightH2: {
+            fontSize: '13px',
+            fontWeight: 'bold',
+            margin: '0',
+            borderBottom: '1px solid #efefef',
+            padding: '20px 0'
+        },
+
+        productionStaffGroupTitle: {
+            margin: '0',
+            borderBottom: '1px dashed #efefef',
+            padding: '20px 0 10px 0',
+            extend: 'productionStaffGroupTitle',
+            '& p': {
+                margin: '0',
+                display: 'inline-block'
+            },
+            '& span': {
+                color: '#999',
+                float: 'right',
+                fontSize: '12px',
+                marginTop: '1px'
+            }
+        },
+        productionStaff:{
+            margin: '0',
+            borderBottom: '1px dashed #efefef',
+            padding: '10px 0 10px 0',
+            extend: 'productionStaff',
+            '& div:first-child': {
+                width: '30px',
+                height: '30px',
+                display: 'inline-block',
+                borderRadius: '50%',
+                verticalAlign: 'top',
+                marginRight: '10px'
+            },
+            '& div:first-child img': {
+                width:'30px'
+            },
+            '& div:last-child': {
+                display: 'inline-block',
+                verticalAlign: 'top'
+            },
+            '& div:last-child span': {
+                color: '#666'
+            }
+        },
+        productionEquipment:{
+            padding: '20px 0',
+            borderBottom: '1px solid #efefef'
+        },
+        productionEquipmentElement:{
+            background: '#f2f5f8',
+            textAlign:'center',
+            padding: '20px 30px'
         }
+
     })
 )
 
@@ -139,9 +239,128 @@ const ManufactureGridList = enhance((props) => {
     return (
         <Container>
             <SubMenu url={ROUTES.MANUFACTURE_LIST_URL}/>
-            <Row>
-                <Col xs={2}>ftest 2</Col>
-                <Col xs={10}>ftest 2</Col>
+            <Row className={classes.productionMainRow}>
+                <Col xs={3} className={classes.productionLeftSide}>
+                    <h2 className={classes.productionH2}>Этапы производства</h2>
+                    <ul className={classes.productionUl}>
+                        <li className={classes.productionTypeLi}>
+                            Производство клея
+                        </li>
+                        <li className={classes.productionTypeLi}>
+                            Производство втулок
+                        </li>
+                        <li className={classes.productionTypeLi}>
+                            Производство рулонов
+                        </li>
+                        <li className={classes.productionTypeLi}>
+                            Резка рулонов
+                        </li>
+                        <li className={classes.productionTypeLi}>
+                            Нанесение логотипа
+                        </li>
+                    </ul>
+                </Col>
+                <Col xs={9} className={classes.productionRightSide}>
+                    <Row>
+                        <Col xs={12}>
+                            <h2 className={classes.productionRightH2}>Производство клея</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={4} style={{borderRight: '1px solid #efefef', height: 'calc(100vh - 120px)', padding: '20px 30px 20px 10px'}}>
+                            <div>
+                                <h3 style={{display: 'inline-block', fontSize: '13px', fontWeight: '600', margin: '0'}}>Персонал</h3>
+                                <a href="#" style={{float: 'right'}}>добавить</a>
+                            </div>
+                            <div style={{marginBottom: '10px'}}>
+                                <div className={classes.productionStaffGroupTitle}>
+                                    <p>Смена А</p>
+                                    <span>График: 09:00 - 18:00</span>
+                                </div>
+                                <ul className={classes.productionUl}>
+                                    <li className={classes.productionStaff}>
+                                        <div>
+                                            <img src />
+                                        </div>
+                                        <div>
+                                            Атамбаев Бекзод<br />
+                                            <span>Должность</span>
+                                        </div>
+                                    </li>
+                                    <li className={classes.productionStaff}>
+                                        <div>
+                                            <img src />
+                                        </div>
+                                        <div>
+                                            Атамбаев Бекзод<br />
+                                            <span>Должность</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div style={{marginBottom: '10px'}}>
+                                <div className={classes.productionStaffGroupTitle}>
+                                    <p>Смена Б</p>
+                                    <span>График: 09:00 - 18:00</span>
+                                </div>
+                                <ul className={classes.productionUl}>
+                                    <li className={classes.productionStaff}>
+                                        <div>
+                                            <img src />
+                                        </div>
+                                        <div>
+                                            Атамбаев Бекзод<br />
+                                            <span>Должность</span>
+                                        </div>
+                                    </li>
+                                    <li className={classes.productionStaff}>
+                                        <div>
+                                            <img src />
+                                        </div>
+                                        <div>
+                                            Атамбаев Бекзод<br />
+                                            <span>Должность</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </Col>
+                        <Col xs={8} style={{padding: '20px 25px'}}>
+                            <Row>
+                                <Col xs={12}>
+                                    <h3 style={{display: 'inline-block', fontSize: '13px', fontWeight: '600', margin: '0'}}>Оборудование</h3>
+                                    <Row className={classes.productionEquipment}>
+                                        <Col xs={4}>
+                                            <div className={classes.productionEquipmentElement}>
+                                                Название оборудования
+                                            </div>
+                                        </Col>
+                                        <Col xs={4}>
+                                            <div className={classes.productionEquipmentElement}>
+                                                Название оборудования
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12}>
+                                    <h3 style={{display: 'inline-block', fontSize: '13px', fontWeight: '600', margin: '0', padding: '20px 0 10px'}}>Список производимой продукции</h3>
+                                </Col>
+                            </Row>
+                            <Row style={{borderBottom: '1px dashed #efefef', padding: '10px 0'}}>
+                                <Col xs={8}>
+                                    <strong>Название продукта</strong><br />
+                                    <span>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</span>
+                                </Col>
+                                <Col xs={4} style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end'}}>
+                                    <a href="#" style={{borderBottom: '1px dashed rgb(18, 170, 235)'}}>BoM </a>
+                                    <a href="#"> ic</a>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
 
         </Container>
