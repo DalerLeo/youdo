43bde60729d600cdb21971407f81f0d71f3d52da
jusss <spectral.ball@gmail.com>
2017-05-08 22:11:44 +0500

    pending expenses grid list filter by date modal is not working yet

---
+++ src/components/PendingExpenses/PendingExpensesCreateDialog.js
@@ -0,0 +1,136 @@
+import _ from 'lodash'
+import React from 'react'
+import PropTypes from 'prop-types'
+import {compose} from 'recompose'
+import injectSheet from 'react-jss'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import CircularProgress from 'material-ui/CircularProgress'
+import {Field, reduxForm, SubmissionError} from 'redux-form'
+import toCamelCase from '../../helpers/toCamelCase'
+import {TextField, PendingExpensesTypeSearchField, BrandSearchField, MeasurementSearchField, ImageUploadField} from '../ReduxForm'
+import CloseIcon2 from '../CloseIcon2'
+import IconButton from 'material-ui/IconButton'
+import MainStyles from '../Styles/MainStyles'
+
+export const PENDING_EXPENSES_CREATE_DIALOG_OPEN = 'openCreateDialog'
+
+const validate = (data) => {
+    const errors = toCamelCase(data)
+    const nonFieldErrors = _.get(errors, 'nonFieldErrors')
+    const latLng = (_.get(errors, 'lat') || _.get(errors, 'lon')) && 'Location is required.'
+
+    throw new SubmissionError({
+        ...errors,
+        latLng,
+        _error: nonFieldErrors
+    })
+}
+const enhance = compose(
+    injectSheet(_.merge(MainStyles, {
+        loader: {
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            background: '#fff',
+            top: '0',
+            left: '0',
+            alignItems: 'center',
+            zIndex: '999',
+            textAlign: 'center',
+            display: ({loading}) => loading ? 'flex' : 'none'
+        }
+    })),
+    reduxForm({
+        form: 'PendingExpensesCreateForm',
+        enableReinitialize: true
+    })
+)
+
+const PendingExpensesCreateDialog = enhance((props) => {
+    const {open, loading, handleSubmit, onClose, classes} = props
+    const onSubmit = handleSubmit(() => props.onSubmit().catch(validate))
+
+    return (
+        <Dialog
+            modal={true}
+            open={open}
+            onRequestClose={onClose}
+            className={classes.dialog}
+            contentStyle={loading ? {width: '300px'} : {}}
+            bodyStyle={{minHeight: 'auto'}}
+            bodyClassName={classes.popUp}>
+            <div className={classes.titleContent}>
+                <span>Добавить продукт</span>
+                <IconButton onTouchTap={onClose}>
+                    <CloseIcon2 color="#666666"/>
+                </IconButton>
+            </div>
+            <div className={classes.bodyContent}>
+                <form onSubmit={onSubmit} className={classes.form}>
+                    <div className={classes.inContent} style={{minHeight: '320px'}}>
+                        <div className={classes.loader}>
+                            <CircularProgress size={80} thickness={5}/>
+                        </div>
+                        <div className={classes.field}>
+                            <Field
+                                name="name"
+                                className={classes.inputField}
+                                component={TextField}
+                                label="Наименование"
+                                fullWidth={true}
+                            />
+                            <Field
+                                name="type"
+                                className={classes.inputField}
+                                component={PendingExpensesTypeSearchField}
+                                label="Тип продукта"
+                                fullWidth={true}
+                            />
+                            <Field
+                                name="brand"
+                                className={classes.inputField}
+                                component={BrandSearchField}
+                                label="Бренд"
+                                fullWidth={true}
+                            />
+                            <Field
+                                name="measurement"
+                                className={classes.inputField}
+                                component={MeasurementSearchField}
+                                label="Мера"
+                                fullWidth={true}
+                            />
+                        </div>
+                        <div className={classes.field} style={{maxWidth: '224px'}}>
+                            <Field
+                                name="image"
+                                className={classes.imageUpload}
+                                component={ImageUploadField}
+                                label="Изображения"
+                                fullWidth={true}
+                            />
+                        </div>
+                    </div>
+                    <div className={classes.bottomButton}>
+                        <FlatButton
+                            label="Сохранить"
+                            className={classes.actionButton}
+                            primary={true}
+                            type="submit"
+                        />
+                    </div>
+                </form>
+            </div>
+        </Dialog>
+    )
+})
+
+PendingExpensesCreateDialog.propTyeps = {
+    open: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
+export default PendingExpensesCreateDialog
---
+++ src/components/PendingExpenses/index.js
@@ -0,0 +1,12 @@
+import PendingExpensesGridList from './PendingExpensesGridList'
+import {PENDING_EXPENSES_CREATE_DIALOG_OPEN} from './PendingExpensesCreateDialog'
+import {PENDING_EXPENSES_UPDATE_DIALOG_OPEN} from './PendingExpensesUpdateDialog'
+import {PENDING_EXPENSES_FILTER_KEY, PENDING_EXPENSES_FILTER_OPEN} from './PendingExpensesFilterForm'
+
+export {
+    PENDING_EXPENSES_CREATE_DIALOG_OPEN,
+    PENDING_EXPENSES_UPDATE_DIALOG_OPEN,
+    PENDING_EXPENSES_FILTER_KEY,
+    PENDING_EXPENSES_FILTER_OPEN,
+    PendingExpensesGridList
+}
