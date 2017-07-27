1bba9c30770e7ffeb9a79c415f70a78139e14f3b
Daler <d.abdullaev@student.inha.uz>
2017-07-27 10:40:26 +0500

    TransactionAccept done, but on wrong branch

--- src/actions/transaction.js
+++ src/actions/transaction.js
@@ -55,7 +55,21 @@ export const transactionCreateSendAction = (formValues, cashboxId) => {
         payload
     }
 }
+export const acceptClientTransactionAction = (clientTransId, cashBoxId) => {
+    const payload = axios()
+        .get(API.ACCEPT_CLIENT_TRANSACTION, {params: {'client_transaction': clientTransId, 'cashbox': cashBoxId}})
+        .then((response) => {
+            return _.get(response, 'data')
+        })
+        .catch((error) => {
+            return Promise.reject(_.get(error, ['response', 'data']))
+        })
 
+    return {
+        type: actionTypes.ACCEPT_CLIENT_TRANSACTION,
+        payload
+    }
+}
 export const transactionDeleteAction = (id) => {
     const payload = axios()
         .delete(sprintf(API.TRANSACTION_DELETE, id))
