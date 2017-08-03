f36633280a294a4ca3b639a7c8e3cdddd9c38996
setuz <set.uzb@gmail.com>
2017-08-03 15:12:32 +0500

    ALL CHANGES

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,9 @@
-export const API_HOST = 'apiparfume.wienerdeming.com'
+export const API_HOST = process.env.API_HOST
 export const API_ROOT = 'api'
 export const API_VERSION = 'v1'
-export const API_URL = `https://${API_HOST}/${API_ROOT}/${API_VERSION}`
+export const API_URL = (API_HOST === 'apistick.wienerdeming.com')
+    ? `https://apistick.wienerdeming.com/${API_ROOT}/${API_VERSION}`
+    : `http://${API_HOST}/${API_ROOT}/${API_VERSION}`
 
 export const USER = 'main/user'
 export const SIGN_IN = `/${USER}/auth/`
