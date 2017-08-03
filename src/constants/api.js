cb0483eb6d90d521ee147a84f362685f12f01f11
setuz <set.uzb@gmail.com>
2017-08-03 08:36:46 +0500

    Merge branch 'master' of https://lab.wienerdeming.com/WDReact/Base into 45-stat-expense
    # Conflicts:
    #	src/constants/actionTypes.js
    #	src/constants/api.js
    #	src/constants/routes.js
    #	src/reducers/index.js
    #	src/routes/index.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,7 @@
 export const API_HOST = process.env.API_HOST
 export const API_ROOT = 'api'
 export const API_VERSION = 'v1'
+export const API_VTEST = 'v1'
 export const API_URL = (API_HOST === 'apiparfume.wienerdeming.com')
     ? `https://apiparfume.wienerdeming.com/${API_ROOT}/${API_VERSION}`
     : `http://${API_HOST}/${API_ROOT}/${API_VERSION}`
