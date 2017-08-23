41431d14d3fd2c071d67da71c2385307ef0b5b8e
Jasur <spectral.ball@gmail.com>
2017-08-23 19:09:41 +0500

    ZONES

--- src/serializers/zoneSerializer.js
+++ src/serializers/zoneSerializer.js
@@ -2,13 +2,15 @@ import _ from 'lodash'
 
 export const createSerializer = (data) => {
     const title = _.get(data, ['zoneName'])
-
+    const coordinates = _.get(data, 'polygon')
+    let newCoordinates = [coordinates]
+    const firstCoordinate = _.first(coordinates)
+    coordinates.push(firstCoordinate)
     return {
         title,
         'coordinates': {
             'type': 'Polygon',
-            'coordinates': [[
-            ]]
+            'coordinates': newCoordinates
         }
     }
 }
