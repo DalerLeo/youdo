986e33d443061d895436277b343b3cca67729512
Daler <d.abdullaev@student.inha.uz>
2017-09-16 13:49:10 +0500

    Stock selection modified in UsersCreate Component

---
+++ src/components/ReduxForm/UserStockRadioButtonField.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import _ from 'lodash'
+import {compose, withState} from 'recompose'
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
+
+const enhance = compose(
+    withState('course', 'setCourse', false)
+)
+
+const UserStockRadioButtonField = enhance((props) => {
+    const {input, stockList} = props
+    return (
+        <RadioButtonGroup name="isPrimary" onChange={input.onChange} defaultSelected={_.toInteger(_.get(input, ['value']))}>
+            {_.map(stockList, (item, index) => {
+                return (
+
+                    <RadioButton
+                        label={_.get(item, 'name')}
+                        key={index}
+                        value={_.toInteger(_.get(item, 'id'))}/>
+                )
+            })}
+        </RadioButtonGroup>
+    )
+})
+
+export default UserStockRadioButtonField
