7de768f7e9be74a4c9368609a2b0fa8e3cd8b1e1
Jasur <spectral.ball@gmail.com>
2017-09-21 15:47:58 +0500

    fixed user select

--- src/helpers/convertCurrency.js
+++ src/helpers/convertCurrency.js
@@ -1,14 +1,20 @@
 import _ from 'lodash'
 import numberWithoutSpaces from './numberWithoutSpaces'
 import numberFormat from './numberFormat'
+import toBoolean from './toBoolean'
 import getConfig from './getConfig'
 
-const convertCurrency = (amount, rate) => {
-    const reversedRate = !getConfig('REVERSED_CURRENCY_RATE')
-    if (reversedRate) {
-        return (numberFormat(_.toNumber(numberWithoutSpaces(amount)) * _.toNumber(numberWithoutSpaces(rate))))
+const primaryIsUSD = toBoolean(getConfig('REVERSED_CURRENCY_RATE'))
+export const convertCurrency = (amount, rate) => {
+    if (primaryIsUSD) {
+        return (numberFormat(_.toNumber(numberWithoutSpaces(amount)) / _.toNumber(numberWithoutSpaces(rate))))
     }
-    return (numberFormat(_.toNumber(numberWithoutSpaces(amount)) / _.toNumber(numberWithoutSpaces(rate))))
+    return (numberFormat(_.toNumber(numberWithoutSpaces(amount)) * _.toNumber(numberWithoutSpaces(rate))))
 }
 
-export default convertCurrency
+export const converToUZS = (amount, rate) => {
+    if (!primaryIsUSD) {
+        return (numberFormat(_.toNumber(numberWithoutSpaces(amount)) / _.toNumber(numberWithoutSpaces(rate))))
+    }
+    return (numberFormat(_.toNumber(numberWithoutSpaces(amount)) * _.toNumber(numberWithoutSpaces(rate))))
+}
