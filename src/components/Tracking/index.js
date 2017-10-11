50c76212827df0a45dddc4da72dad6286ff07021
Jasur <spectral.ball@gmail.com>
2017-10-11 13:52:24 +0500

    fixes

--- src/components/Tracking/index.js
+++ src/components/Tracking/index.js
@@ -1,11 +1,9 @@
 import TrackingWrapper from './TrackingWrapper'
-const ADD_TRACKING = 'openAddTracking'
 const OPEN_SHOP_DETAILS = 'openShop'
 const USER_GROUP = 'group'
 const DATE = 'date'
 
 export {
-    ADD_TRACKING,
     OPEN_SHOP_DETAILS,
     TrackingWrapper,
     USER_GROUP,
