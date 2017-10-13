4be8b98d7f7197819eff794e0aa49b02f8e20a57
setuz <set.uzb@gmail.com>
2017-10-13 17:31:10 +0500

    Merge branch 'master' of https://lab.wienerdeming.com/WDReact/Base
    # Conflicts:
    #	src/components/Return/ReturnDetails.js

--- src/components/Manufacture/ManufactureAddStaffDialog.js
+++ src/components/Manufacture/ManufactureAddStaffDialog.js
@@ -132,6 +132,7 @@ const ManufactureAddStaffDialog = enhance((props) => {
         open,
         loading,
         onClose,
+        isUpdate,
         handleSubmit,
         classes
     } = props
