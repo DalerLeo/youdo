d6e9a061eabdba19fe40b6f971dff2a16c7020cd
Jasur <spectral.ball@gmail.com>
2017-11-04 16:39:57 +0500

    manufacture activity

--- src/serializers/shipmentSerializer.js
+++ src/serializers/shipmentSerializer.js
@@ -9,17 +9,31 @@ export const createSerializer = (data) => {
     }
 }
 
-export const listFilterSerializer = (data, manufacture) => {
+export const listFilterSerializer = (data, manufacture, dateRange) => {
     const {...defaultData} = data
     const ordering = _.get(data, 'ordering')
 
     return {
+        'begin_date': _.get(dateRange, 'beginDate'),
+        'end_date': _.get(dateRange, 'endDate'),
         'manufacture': manufacture,
         'name': _.get(defaultData, 'name'),
         'search': _.get(defaultData, 'search'),
         'page': _.get(defaultData, 'page'),
         'page_size': _.get(defaultData, 'pageSize'),
         'ordering': ordering && orderingSnakeCase(ordering)
+    }
+}
+
+export const logsFilterSerializer = (data, manufacture, dateRange) => {
+    const {...defaultData} = data
+
+    return {
+        'begin_date': _.get(dateRange, 'beginDate'),
+        'end_date': _.get(dateRange, 'endDate'),
+        'manufacture': manufacture,
+        'page': _.get(defaultData, 'logsPage'),
+        'page_size': _.get(defaultData, 'logsPageSize')
     }
 }
 
