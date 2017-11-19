ee8517455dfca776aa1e5e04dec48e21f7ed7dcb
setuz <set.uzb@gmail.com>
2017-11-19 14:06:50 +0500

    Merge branch 'master' of https://lab.wienerdeming.com/WDReact/Base into 308-order-bug
    # Conflicts:
    #	src/components/Order/OrderGridList.js
    #	src/containers/Order/OrderList.js

--- src/components/Statistics/ClientIncome/ClientIncomeGridList.js
+++ src/components/Statistics/ClientIncome/ClientIncomeGridList.js
@@ -27,195 +27,195 @@ import ClientBalanceFormat from './ClientBalanceFormat'
 const NEGATIVE = -1
 
 export const CLIENT_INCOME_FILTER_KEY = {
-  FROM_DATE: 'fromDate',
-  TO_DATE: 'toDate',
-  SEARCH: 'search',
-  DIVISION: 'division',
-  TYPE: 'type',
-  CLIENT: 'client'
+    FROM_DATE: 'fromDate',
+    TO_DATE: 'toDate',
+    SEARCH: 'search',
+    DIVISION: 'division',
+    TYPE: 'type',
+    CLIENT: 'client'
 }
 
 const enhance = compose(
   injectSheet({
-    green: {
-      color: '#81c784'
-    },
-    red: {
-      color: '#e57373'
-    },
-    mainWrapper: {
-      background: '#fff',
-      margin: '0 -28px',
-      height: 'calc(100% + 28px)',
-      boxShadow: 'rgba(0, 0, 0, 0.09) 0px -1px 6px, rgba(0, 0, 0, 0.10) 0px -1px 4px'
-    },
-    wrapper: {
-      height: '100%',
-      overflowY: 'auto',
-      padding: '20px 30px',
-      '& .row': {
-        marginLeft: '0',
-        marginRight: '0'
-      }
-    },
-    loader: {
-      width: '100%',
-      padding: '100px 0',
-      background: '#fff',
-      justifyContent: 'center',
-      alignItems: 'center',
-      zIndex: '999',
-      display: 'flex'
-    },
-    graphLoader: {
-      extend: 'loader',
-      height: '160px',
-      marginTop: '30px',
-      padding: '0'
-    },
-    pagination: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      borderTop: '1px #efefef solid',
-      borderBottom: '1px #efefef solid'
-    },
-    tableWrapper: {
-      '& .row': {
-        '&:after': {
-          bottom: '-1px'
-        },
-        '& > div': {
-          '&:first-child': {
-            paddingLeft: '0'
-          },
-          '&:last-child': {
-            textAlign: 'right',
-            paddingRight: '0'
-          }
-        }
+      green: {
+          color: '#81c784'
       },
-      '& .dottedList': {
-        padding: '5px 0',
-        minHeight: '50px',
-        '&:last-child:after': {
-          content: '""',
-          backgroundImage: 'none'
-        },
-        '& a': {
-          fontWeight: '600'
-        }
+      red: {
+          color: '#e57373'
       },
-      '& .personImage': {
-        borderRadius: '50%',
-        overflow: 'hidden',
-        height: '30px',
-        minWidth: '30px',
-        width: '30px',
-        marginRight: '10px',
-        '& img': {
-          display: 'flex',
+      mainWrapper: {
+          background: '#fff',
+          margin: '0 -28px',
+          height: 'calc(100% + 28px)',
+          boxShadow: 'rgba(0, 0, 0, 0.09) 0px -1px 6px, rgba(0, 0, 0, 0.10) 0px -1px 4px'
+      },
+      wrapper: {
           height: '100%',
-          width: '100%'
-        }
-      }
-    },
-    balanceInfo: {
-      padding: '15px 0'
-    },
-    balance: {
-      paddingRight: '10px',
-      fontSize: '24px!important',
-      fontWeight: '600'
-    },
-    inputFieldCustom: {
-      fontSize: '13px !important',
-      height: '45px !important',
-      marginTop: '7px',
-      '& div': {
-        fontSize: '13px !important'
+          overflowY: 'auto',
+          padding: '20px 30px',
+          '& .row': {
+              marginLeft: '0',
+              marginRight: '0'
+          }
       },
-      '& label': {
-        top: '20px !important',
-        lineHeight: '5px !important'
+      loader: {
+          width: '100%',
+          padding: '100px 0',
+          background: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: '999',
+          display: 'flex'
       },
-      '& input': {
-        marginTop: '0 !important'
-      }
-    },
-    balanceButtonWrap: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between'
-    },
-    leftPanel: {
-      backgroundColor: '#f2f5f8',
-      flexBasis: '250px',
-      maxWidth: '250px'
+      graphLoader: {
+          extend: 'loader',
+          height: '160px',
+          marginTop: '30px',
+          padding: '0'
+      },
+      pagination: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          borderTop: '1px #efefef solid',
+          borderBottom: '1px #efefef solid'
+      },
+      tableWrapper: {
+          '& .row': {
+              '&:after': {
+                  bottom: '-1px'
+              },
+              '& > div': {
+                  '&:first-child': {
+                      paddingLeft: '0'
+                  },
+                  '&:last-child': {
+                      textAlign: 'right',
+                      paddingRight: '0'
+                  }
+              }
+          },
+          '& .dottedList': {
+              padding: '5px 0',
+              minHeight: '50px',
+              '&:last-child:after': {
+                  content: '""',
+                  backgroundImage: 'none'
+              },
+              '& a': {
+                  fontWeight: '600'
+              }
+          },
+          '& .personImage': {
+              borderRadius: '50%',
+              overflow: 'hidden',
+              height: '30px',
+              minWidth: '30px',
+              width: '30px',
+              marginRight: '10px',
+              '& img': {
+                  display: 'flex',
+                  height: '100%',
+                  width: '100%'
+              }
+          }
+      },
+      balanceInfo: {
+          padding: '15px 0'
+      },
+      balance: {
+          paddingRight: '10px',
+          fontSize: '24px!important',
+          fontWeight: '600'
+      },
+      inputFieldCustom: {
+          fontSize: '13px !important',
+          height: '45px !important',
+          marginTop: '7px',
+          '& div': {
+              fontSize: '13px !important'
+          },
+          '& label': {
+              top: '20px !important',
+              lineHeight: '5px !important'
+          },
+          '& input': {
+              marginTop: '0 !important'
+          }
+      },
+      balanceButtonWrap: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+      },
+      leftPanel: {
+          backgroundColor: '#f2f5f8',
+          flexBasis: '250px',
+          maxWidth: '250px'
 
-    },
-    rightPanel: {
-      flexBasis: 'calc(100% - 250px)',
-      maxWidth: 'calc(100% - 250px)',
-      overflow: 'hidden'
-    },
-    diagram: {
-      marginTop: '20px',
-      '& > div:first-child': {
-        paddingLeft: '0'
       },
-      '& > div:last-child': {
-        paddingRight: '0'
-      }
-    },
-    summaryTitle: {
-      color: '#666'
-    },
-    summaryValue: {
-      fontWeight: '600',
-      fontSize: '16px',
-      marginBottom: '10px',
-      '&:last-child': {
-        margin: '0'
-      }
-    },
-    mainSummary: {
-      '& > div:last-child': {
-        borderBottom: '1px #efefef solid',
-        paddingBottom: '10px'
-      }
-    },
-    secondarySummary: {
-      margin: '10px 0',
-      '& span': {
-        display: 'block'
+      rightPanel: {
+          flexBasis: 'calc(100% - 250px)',
+          maxWidth: 'calc(100% - 250px)',
+          overflow: 'hidden'
       },
-      '& > div': {
-        fontSize: '16px'
+      diagram: {
+          marginTop: '20px',
+          '& > div:first-child': {
+              paddingLeft: '0'
+          },
+          '& > div:last-child': {
+              paddingRight: '0'
+          }
+      },
+      summaryTitle: {
+          color: '#666'
+      },
+      summaryValue: {
+          fontWeight: '600',
+          fontSize: '16px',
+          marginBottom: '10px',
+          '&:last-child': {
+              margin: '0'
+          }
+      },
+      mainSummary: {
+          '& > div:last-child': {
+              borderBottom: '1px #efefef solid',
+              paddingBottom: '10px'
+          }
+      },
+      secondarySummary: {
+          margin: '10px 0',
+          '& span': {
+              display: 'block'
+          },
+          '& > div': {
+              fontSize: '16px'
+          }
+      },
+      chart: {
+          '& .highcharts-label': {
+              boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px !important'
+          }
+      },
+      emptyQuery: {
+          background: 'url(' + NotFound + ') no-repeat center center',
+          backgroundSize: '200px',
+          padding: '200px 0 0',
+          textAlign: 'center',
+          fontSize: '13px',
+          color: '#666'
       }
-    },
-    chart: {
-      '& .highcharts-label': {
-        boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px !important'
-      }
-    },
-    emptyQuery: {
-      background: 'url(' + NotFound + ') no-repeat center center',
-      backgroundSize: '200px',
-      padding: '200px 0 0',
-      textAlign: 'center',
-      fontSize: '13px',
-      color: '#666'
-    }
   }),
   reduxForm({
-    form: 'StatisticsFilterForm',
-    enableReinitialize: true
+      form: 'StatisticsFilterForm',
+      enableReinitialize: true
   })
 )
 
 const ClientIncomeGridList = enhance((props) => {
-  const {
+    const {
     graphData,
     classes,
     filter,
@@ -226,39 +226,39 @@ const ClientIncomeGridList = enhance((props) => {
     listData
   } = props
 
-  const divisionStatus = getConfig('DIVISIONS')
-  const graphLoading = _.get(graphData, 'graphInLoading') || _.get(graphData, 'graphOutLoading')
-  const loading = _.get(listData, 'listLoading')
-  const primaryCurrency = getConfig('PRIMARY_CURRENCY')
-  const sumIn = _.sumBy(_.get(graphData, 'dataIn'), (item) => {
-    return _.toNumber(_.get(item, 'amount'))
-  })
-  const valueInName = _.map(_.get(graphData, 'dataIn'), (item) => {
-    return _.get(item, 'date')
-  })
-  const valueOutName = _.map(_.get(graphData, 'dataOut'), (item) => {
-    return _.get(item, 'date')
-  })
-  const tooltipDate = valueInName.length < valueOutName.length ? valueOutName : valueInName
+    const divisionStatus = getConfig('DIVISIONS')
+    const graphLoading = _.get(graphData, 'graphInLoading') || _.get(graphData, 'graphOutLoading')
+    const loading = _.get(listData, 'listLoading')
+    const primaryCurrency = getConfig('PRIMARY_CURRENCY')
+    const sumIn = _.sumBy(_.get(graphData, 'dataIn'), (item) => {
+        return _.toNumber(_.get(item, 'amount'))
+    })
+    const valueInName = _.map(_.get(graphData, 'dataIn'), (item) => {
+        return _.get(item, 'date')
+    })
+    const valueOutName = _.map(_.get(graphData, 'dataOut'), (item) => {
+        return _.get(item, 'date')
+    })
+    const tooltipDate = valueInName.length < valueOutName.length ? valueOutName : valueInName
 
-  const sumOut = _.sumBy(_.get(graphData, 'dataOut'), (item) => {
-    return _.toNumber(_.get(item, 'amount')) * NEGATIVE
-  })
-  const valueIn = _.map(_.get(graphData, 'dataIn'), (item) => {
-    return _.toNumber(_.get(item, 'amount'))
-  })
-  const valueOut = _.map(_.get(graphData, 'dataOut'), (item) => {
-    return _.toNumber(_.get(item, 'amount')) * NEGATIVE
-  })
+    const sumOut = _.sumBy(_.get(graphData, 'dataOut'), (item) => {
+        return _.toNumber(_.get(item, 'amount')) * NEGATIVE
+    })
+    const valueIn = _.map(_.get(graphData, 'dataIn'), (item) => {
+        return _.toNumber(_.get(item, 'amount'))
+    })
+    const valueOut = _.map(_.get(graphData, 'dataOut'), (item) => {
+        return _.toNumber(_.get(item, 'amount')) * NEGATIVE
+    })
 
-  const diff = sumIn - sumOut
-  const headerStyle = {
-    backgroundColor: '#fff',
-    fontWeight: '600',
-    color: '#666'
-  }
+    const diff = sumIn - sumOut
+    const headerStyle = {
+        backgroundColor: '#fff',
+        fontWeight: '600',
+        color: '#666'
+    }
 
-  const headers = (
+    const headers = (
     <Row style={headerStyle} className="dottedList">
       <Col xs={1}>№</Col>
       <Col xs={2}>Дата</Col>
@@ -269,22 +269,22 @@ const ClientIncomeGridList = enhance((props) => {
     </Row>
   )
 
-  const ZERO = 0
-  const list = _.map(_.get(listData, 'data'), (item, index) => {
-    const transId = _.get(item, 'id')
-    const user = _.get(item, 'user')
-    const comment = _.get(item, 'comment')
-    const client = _.get(item, ['client', 'name'])
-    const currency = _.get(item, ['currency', 'name'])
-    const userName = !_.isNull(user) ? user.firstName + ' ' + user.secondName : 'Не известно'
-    const date = dateFormat(_.get(item, 'createdDate')) + ' ' + moment(_.get(item, 'createdDate')).format('HH:mm')
-    const amount = _.toNumber(_.get(item, 'amount'))
-    const internal = _.toNumber(_.get(item, 'internal'))
-    const customRate = _.get(item, 'customRate') ? _.toInteger(_.get(item, 'customRate')) : _.toInteger(amount / internal)
-    const type = _.get(item, 'type')
-    const orderId = _.get(item, 'order')
-    const orderReturnId = _.get(item, 'orderReturn')
-    return (
+    const ZERO = 0
+    const list = _.map(_.get(listData, 'data'), (item, index) => {
+        const transId = _.get(item, 'id')
+        const user = _.get(item, 'user')
+        const comment = _.get(item, 'comment')
+        const client = _.get(item, ['client', 'name'])
+        const currency = _.get(item, ['currency', 'name'])
+        const userName = !_.isNull(user) ? user.firstName + ' ' + user.secondName : 'Не известно'
+        const date = dateFormat(_.get(item, 'createdDate')) + ' ' + moment(_.get(item, 'createdDate')).format('HH:mm')
+        const amount = _.toNumber(_.get(item, 'amount'))
+        const internal = _.toNumber(_.get(item, 'internal'))
+        const customRate = _.get(item, 'customRate') ? _.toInteger(_.get(item, 'customRate')) : _.toInteger(amount / internal)
+        const type = _.get(item, 'type')
+        const orderId = _.get(item, 'order')
+        const orderReturnId = _.get(item, 'orderReturn')
+        return (
       <Row key={index} className="dottedList">
         <Col xs={1}>{transId}</Col>
         <Col xs={2}>{date}</Col>
@@ -304,10 +304,10 @@ const ClientIncomeGridList = enhance((props) => {
           </div>
         </Col>
       </Row>
-    )
-  })
+        )
+    })
 
-  const fields = (
+    const fields = (
     <div>
       <Field
         className={classes.inputFieldCustom}
@@ -336,7 +336,7 @@ const ClientIncomeGridList = enhance((props) => {
     </div>
   )
 
-  const page = (
+    const page = (
     <div className={classes.mainWrapper}>
       <Row style={{margin: '0', height: '100%'}}>
         <div className={classes.leftPanel}>
@@ -414,17 +414,17 @@ const ClientIncomeGridList = enhance((props) => {
     </div>
   )
 
-  return (
+    return (
     <Container>
       {page}
     </Container>
-  )
+    )
 })
 
 ClientIncomeGridList.propTypes = {
-  filter: PropTypes.object.isRequired,
-  listData: PropTypes.object,
-  detailData: PropTypes.object
+    filter: PropTypes.object.isRequired,
+    listData: PropTypes.object,
+    detailData: PropTypes.object
 }
 
 export default ClientIncomeGridList
