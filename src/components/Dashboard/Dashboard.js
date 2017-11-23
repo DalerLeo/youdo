75d7785d0ad3512134f1741c592d931070f9ab82
Jasur <spectral.ball@gmail.com>
2017-11-23 10:40:39 +0500

    merge qivor

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,129 +1,76 @@
-import _ from 'lodash'
 import React from 'react'
-import {compose} from 'recompose'
 import injectSheet from 'react-jss'
-import Container from '../Container'
-import Paper from 'material-ui/Paper'
-import User from '../Images/person.png'
-import Filter from './Filter'
-import Widgets from './Widgets'
-import OrderChart from './OrderChart'
+import {compose} from 'recompose'
+import ChoosMenu from '../../components/Images/choose-menu.png'
+import Layout from '../../components/Layout'
 
 const enhance = compose(
     injectSheet({
         wrapper: {
-            position: 'absolute',
-            left: '-28px',
-            right: '-28px',
-            top: '0',
-            bottom: '-28px',
-            padding: '20px 30px',
-            overflowY: 'auto'
-        },
-        header: {
-            padding: '20px 30px',
+            position: 'fixed',
+            left: '84px',
+            right: '0',
+            background: '#fdfdfd',
+            height: '100%',
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'space-between',
-            maxHeight: '70px'
+            justifyContent: 'center',
+            userSelect: 'none'
         },
-        user: {
-            display: 'flex',
-            alignItems: 'center',
-            '& div': {
-                marginRight: '10px',
-                fontSize: '16px',
-                fontWeight: '600'
-            },
-            '& span': {
-                color: '#999',
-                fontSize: '12px',
-                fontWeight: '600',
-                marginLeft: '-5px'
-            }
-        },
-        userImage: {
-            background: 'url(' + User + ') no-repeat center center',
-            backgroundSize: 'cover',
-            width: '30px',
-            height: '30px',
-            borderRadius: '50%',
-            overflow: 'hidden'
-        },
-        buttons: {
+        item: {
+            padding: '135px 100px',
+            background: '#fff',
+            border: '1px #efefef solid',
             display: 'flex',
             alignItems: 'center'
         },
-        chartsWrapper: {
-            marginTop: '20px',
-            display: 'flex',
-            justifyContent: 'space-between',
-            flexWrap: 'wrap',
-            '& > div': {
-                width: 'calc(50% - 10px)',
-                marginBottom: '20px',
-                padding: '20px 30px',
-                background: '#efefef'
-            }
+        image: {
+            background: 'url(' + ChoosMenu + ') no-repeat center center',
+            width: '200px',
+            height: '200px',
+            marginRight: '60px'
         },
-        chartHeader: {
-            padding: '15px 30px',
-            margin: '-20px -30px 20px',
-            borderBottom: '1px #efefef solid',
-            fontWeight: '600'
+        text: {
+            color: '#666',
+            '& h1': {
+                fontSize: '48px',
+                lineHeight: '1',
+                fontWeight: '600',
+                marginBottom: '30px',
+                '& span': {
+                    display: 'block',
+                    fontSize: '25px !important'
+                }
+            },
+            '& p': {
+                fontSize: '14px',
+                margin: '5px 0',
+                fontWeight: '600',
+                '& a': {
+                    fontWeight: '600'
+                }
+            }
         }
     })
 )
 
-const Dashboard = enhance((props) => {
-    const {
-        classes,
-        filter,
-        orderChart,
-        dateInitialValues
-    } = props
-    const orderChartLoading = _.get(orderChart, 'loading')
-    const orderChartDate = _.map(_.get(orderChart, 'data'), (item) => {
-        return _.get(item, 'date')
-    })
-    const orderChartTotal = _.map(_.get(orderChart, 'data'), (item) => {
-        return _.toNumber(_.get(item, 'amount'))
-    })
+const MainList = enhance((props) => {
+    const {classes, layout} = props
     return (
-        <Container>
+        <Layout {...layout}>
             <div className={classes.wrapper}>
-                <Paper zDepth={1} className={classes.header}>
-                    <div className={classes.user}>
-                        <div className={classes.userImage}>{null}</div>
-                        <div>Jasur Juraev</div>
-                        <span>(Admin)</span>
+                <div className={classes.item}>
+                    <div className={classes.image}>
                     </div>
-                    <div className={classes.buttons}>
-                        <Filter filter={filter} initialValues={dateInitialValues}/>
-                        <Widgets/>
-                    </div>
-                </Paper>
 
-                <section className={classes.chartsWrapper}>
-                    <Paper zDepth={1}>
-                        <div className={classes.chartHeader}>
-                            <div>Продажи</div>
-                        </div>
-                        {orderChartLoading
-                        ? <div>Loading</div>
-                        : <OrderChart
-                            height={250}
-                            primaryText="Нал"
-                            secondaryText="Переч"
-                            primaryValues={orderChartTotal}
-                            secondaryValues={orderChartTotal}
-                            tooltipTitle={orderChartDate}
-                        />}
-                    </Paper>
-                </section>
+                    <div className={classes.text}>
+                        <h1>Добро пожаловать!</h1>
+                        <p>Для работы с системой выберите пункт меню</p>
+                    </div>
+                </div>
             </div>
-        </Container>
+        </Layout>
     )
 })
 
-export default Dashboard
+export default MainList
